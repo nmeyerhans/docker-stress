7a5874d911e43b6258734a0e41dd9b2fd24cd35d
Noah Meyerhans <nmeyerha@amazon.com>
2017-03-06 13:27:23 -0800

    Add CPU stressers

--- stress.sh
+++ stress.sh
@@ -4,7 +4,8 @@ if [ -n "$TIMEOUT" ]; then
     TIMEOUT_OPT="--timeout $TIMEOUT"
 fi
 
-stress --hdd ${HDD_THREADS:-2} \
+stress --hdd ${HDD_THREADS:-1} \
        --hdd-bytes ${HDD_BYTES:-100K} \
        --io ${IO_THREADS:-1} \
+       --cpu ${CPU_THREADS:-1} \
        $TIMEOUT_OPT
