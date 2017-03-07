b9aafbfe835f1bf206ddecc57fcf282e45da7eaa
Noah Meyerhans <nmeyerha@amazon.com>
2017-03-07 14:38:42 -0800

    Make all the stressers optional; Log runtime.

--- stress.sh
+++ stress.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
+start_time=$(date +%s)
+
 if [ -n "$TIMEOUT" ]; then
     TIMEOUT_OPT="--timeout $TIMEOUT"
 fi
 
-stress --hdd ${HDD_THREADS:-1} \
-       --hdd-bytes ${HDD_BYTES:-100K} \
-       --io ${IO_THREADS:-1} \
-       --cpu ${CPU_THREADS:-1} \
+if [ -n "$HDD_THREADS" ]; then
+    HDD_OPT="--hdd $HDD_THREADS"
+fi
+
+if [ -n "$HDD_BYTES" ]; then
+    HDD_BYTES_OPT="--hdd-bytes $HDD_BYTES"
+fi
+
+if [ -n "$IO_THREADS" ]; then
+    IO_THREADS_OPT="--io $IO_THREADS"
+fi
+
+if [ -n "$CPU_THREADS" ]; then
+    CPU_THREADS_OPT="--cpu $CPU_THREADS"
+fi
+
+stress $HDD_OPT \
+       $HDD_BYTES_OPT \
+       $IO_THREADS_OPT \
+       $CPU_THREADS_OPT \
        $TIMEOUT_OPT
+
+end_time=$(date +%s)
+echo "Finished after $(($end_time-$start_time))s"
